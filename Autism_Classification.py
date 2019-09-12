afdc83fabcdbd667e5cd4f6d37d5aaca929e8392
Sazzadur Rahman <sazzadur.rahman@gmail.com>
2019-09-12 18:20:44 +0100

    To determine whether a patient has ASD or not

--- Autism_Classification.py
+++ Autism_Classification.py
@@ -4,17 +4,10 @@
 # ### Machine Learning (SVM, Logistic Regression, Randon Forest)
 # ### Autism classification
 
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
-# In[48]:
-
 
 
 print("Setup starts...")
